fa1f0e179da5e5dc1db3c60bf2b9a5b91d03e858
tiagomp636 <tiagomdepaula@gmail.com>
2015-07-22 12:04:47 -0300

    fix caminho para framework

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
+
 // change the following paths if necessary
-$yii = dirname(__FILE__) . '/../monitor-src/framework/yiilite.php';
+if (YII_DEBUG) {
+    $yii = dirname(__FILE__) . '/../monitor-src/framework/yiilite.php';
+} else {
+    $yii = dirname(__FILE__) . '/monitor-src/framework/yiilite.php';
+}
 $config = dirname(__FILE__) . '/protected/config/main.php';
 
 // remove the following lines when in production mode
-defined('YII_DEBUG') or define('YII_DEBUG', in_array($_SERVER['REMOTE_ADDR'], array('localhost', '192.168.0.104', '::1','127.0.0.1')));
+defined('YII_DEBUG') or define('YII_DEBUG', in_array($_SERVER['REMOTE_ADDR'], array('localhost', '192.168.0.104', '::1', '127.0.0.1')));
 //define('YII_DEBUG', true);
-
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL', 3);
 
