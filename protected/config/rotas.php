babfd0e5f50ecedec2166f90d5f264800417571d
Tiago M. de Paula <tiagomdepaula@gmail.com>
2015-07-30 17:36:41 -0300

    url amigabel para processamento de bsuca

--- protected/config/rotas.php
+++ protected/config/rotas.php
@@ -2,10 +2,9 @@
 
 return array(
     '/' => 'site/index',
-    '/do/<q>' => 'search/ResultEq',
     '<action:(login|logout)>' => 'site/<action>',
     'topico/<id:\d+>/<nome>' => 'topico/ver',
     'topico/<id>/<nome>' => 'topico/ver',
     'play/<id>' => 'play/index',
-    '/do' => 'search/ResultEq',
+    '/do/<q>' => 'search/ResultEq',
 );
