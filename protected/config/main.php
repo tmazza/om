4df3cb33231851d3d9c80ecbfeb8f637ee85f35b
Tiago M. de Paula <tiagomdepaula@gmail.com>
2015-07-30 18:43:13 -0300

    index.php sempre presente

--- protected/config/main.php
+++ protected/config/main.php
@@ -42,7 +42,7 @@ return array(
         'urlManager' => array(
             'urlFormat' => 'path',
             'caseSensitive' => true,
-            'showScriptName' => YII_DEBUG,
+            'showScriptName' => true,
             'rules' => require(dirname(__FILE__) . '/rotas.php'),
         ),
         'db' => require(dirname(__FILE__) . '/dbMonitor.php'),
