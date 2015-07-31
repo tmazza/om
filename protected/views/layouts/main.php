c16fa11ca3f3ff37ceb9f8d3fecb7e02f10db073
Tiago M <tiagompdepaula@gmail.com>
2015-07-31 00:57:11 -0300

    imagem para compartilhamento

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -7,6 +7,7 @@
         <!-- META TAGS -->
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="og:image" content="webroot/logo-mini.png" />
         <title>O Monitor</title>
         <link rel="shortcut icon" href="<?php echo Yii::app()->baseUrl ?>/webroot/monitor/images/favicon.png?v=2" />
 
