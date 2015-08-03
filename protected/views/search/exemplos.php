a5591e6a44c9ffc2c2c5829712f9af3b651fa266
Tiago M. de Paula <tiagomdepaula@gmail.com>
2015-08-03 15:16:52 -0300

    layout de exemplos da busca no resultado da serch

--- protected/views/search/exemplos.php
+++ protected/views/search/exemplos.php
@@ -4,8 +4,13 @@
 	<h4><?=$nome;?></h4>
 	<?php foreach ($exemplos as $e): ?>
 		<a class="card" href="<?= $this->createUrl('search/ResultEq', array('q' => $e->valor)); ?>" style="max-width: 260px;" >
-			<span class="card-label">$$<?= $e->latex; ?>$$</span>
-			<span class="card-value"><?= $e->valor; ?></span>
+			<?php if($e->layout == ExemplosSearch::LayHor): ?>
+				<span class="card-label-h">$$<?= $e->latex; ?>$$</span>
+				<span class="card-value-h"><?= $e->valor; ?></span>				
+			<?php else: ?>
+				<span class="card-label">$$<?= $e->latex; ?>$$</span>
+				<span class="card-value"><?= $e->valor; ?></span>
+			<?php endif; ?>
 		</a>    
 	<?php endforeach; ?>
 	<br>
