6844624b0425c957ea0aa87280f475a08e2b5f31
tmazza <tiagomdepaula@gmail.com>
2015-08-12 18:47:00 -0300

    nova index

--- protected/views/search/exemplos.php
+++ protected/views/search/exemplos.php
@@ -3,15 +3,15 @@
 	<div style="margin-right:12px" class="pull-right btn btn-danger btn-mini" onclick="$(this).parent().parent().slideUp().html('');">x</div>
 	<h4><?=$nome;?></h4>
 	<?php foreach ($exemplos as $e): ?>
-		<a class="card" href="<?= $this->createUrl('search/ResultEq', array('q' => $e->valor)); ?>" style="max-width: 260px;" >
+		<a class="card" href="<?= $this->createUrl('search/ResultEq', array('q' => $e->valor)); ?>" style="max-width: 360px;" >
 			<?php if($e->layout == ExemplosSearch::LayHor): ?>
 				<span class="card-label-h">$$<?= $e->latex; ?>$$</span>
-				<span class="card-value-h"><?= $e->valor; ?></span>				
+				<span class="card-value-h"><?= $e->valor; ?></span>
 			<?php else: ?>
 				<span class="card-label">$$<?= $e->latex; ?>$$</span>
 				<span class="card-value"><?= $e->valor; ?></span>
 			<?php endif; ?>
-		</a>    
+		</a>
 	<?php endforeach; ?>
 	<br>
 </div>
--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -1,25 +1,26 @@
-<div class="container textCenter">
+<div class="container">
 	<br><br>
 	<?php $count = 1; ?>
 	<?php foreach ($cats as $c): ?>
-		<?= ($count % 3 == 0) ? '<div class="row">' : null; ?>
-		<div class="span4 card-container">
-		<p style="color: #555;text-align:center;font-size:20px;" class='card-container-title'><?=$c->nome;?></p>
-		<div class="card-box">
+	<div class="row">
+		<div class="span12 card-container">
+		<br>
+		<p style="color: #555;font-size:14px;" class='card-container-title'><?=$c->nome;?></p>
+		<div class="">
+		<br>
 		<?php foreach ($c->exemplos as $e): ?>
-			<a class="card" href="<?= $this->createUrl('search/ResultEq', array('q' => $e->valor)); ?>">
-				<?php if($e->layout == ExemplosSearch::LayHor): ?>
-					<span class="card-label-h">$$<?= $e->latex; ?>$$</span>
-					<span class="card-value-h"><?= $e->valor; ?></span>				
-				<?php else: ?>
-					<span class="card-label">$$<?= $e->latex; ?>$$</span>
-					<span class="card-value"><?= $e->valor; ?></span>
-				<?php endif; ?>
-			</a>    
+			<a class="card-micro" href="<?= $this->createUrl('search/ResultEq', array('q' => $e->valor)); ?>">
+					<div class='l' style="display:inline-block;">$<?= $e->latex; ?>$</div>
+					<?php if($e->layout == ExemplosSearch::LayHor): ?>
+						<div style=""><?= $e->valor; ?></div>
+					<?php else:; ?>
+						<div style="float: right;"><?= $e->valor; ?></div>
+					<?php endif; ?>
+			</a>
 		<?php endforeach; ?>
 		</div>
 		<br><br>
-		<?= ($count % 3 == 0) ? '</div>' : null; ?>
+		</div>
 		</div>
 	<?php $count++; ?>
     <?php endforeach; ?>
