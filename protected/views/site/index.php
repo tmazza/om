e196624685cbd58949fe153e34d484065fee3dcc
tmazza <tiagomdepaula@gmail.com>
2015-08-13 21:37:27 -0300

    layout para mobile modificado

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,22 +4,20 @@
 	<?php foreach ($cats as $c): ?>
 	<div class="row">
 		<div class="span12 card-container">
-		<br>
-		<p style="color: #555;font-size:14px;" class='card-container-title'><?=$c->nome;?></p>
-		<div class="">
-		<br>
-		<?php foreach ($c->exemplos as $e): ?>
-			<a class="card-micro" href="<?= $this->createUrl('search/ResultEq', array('q' => $e->valor)); ?>">
-					<div class='l' style="display:inline-block;">$<?= $e->latex; ?>$</div>
-					<?php if($e->layout == ExemplosSearch::LayHor): ?>
-						<div style="text-align:right"><?= $e->valor; ?></div>
-					<?php else:; ?>
-						<div style="float: right;"><?= $e->valor; ?></div>
-					<?php endif; ?>
-			</a>
-		<?php endforeach; ?>
+		<p style="cursor:pointer;color: #555;font-size:14px;" onclick='$(this).next().slideToggle();' class='card-container-title'><?=$c->nome;?></p>
+		<div class='hide-mob'>
+			<?php foreach ($c->exemplos as $e): ?>
+				<a class="card-micro" href="<?= $this->createUrl('search/ResultEq', array('q' => $e->valor)); ?>">
+						<div class='l' style="display:inline-block;">$<?= $e->latex; ?>$</div>
+						<?php if($e->layout == ExemplosSearch::LayHor): ?>
+							<div style="text-align:right"><?= $e->valor; ?></div>
+						<?php else:; ?>
+							<div style="float: right;"><?= $e->valor; ?></div>
+						<?php endif; ?>
+				</a>
+			<?php endforeach; ?>
 		</div>
-		<br><br>
+		<br>
 		</div>
 		</div>
 	<?php $count++; ?>
