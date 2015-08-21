74e2d835d5c21379a1b657ba8b61845ee7a82b66
tmazza <tiagomdepaula@gmail.com>
2015-08-21 17:37:00 -0300

    subniveis no menu apos 1 resolução

--- protected/views/layouts/search.php
+++ protected/views/layouts/search.php
@@ -29,7 +29,7 @@
 			<meta property="og:title" content="<?=isset($_GET['q']) ? $_GET['q'] : null; ?>"/>
 			<meta property="og:image" content="http://omonitor.io/dev/monitor-lite/webroot/logo-face3.png"/>
 			<meta property="og:site_name" content="O Monitor"/>
-			<meta property="og:description" content="Calcule problemas matemáticos em diversas áreas: ... - O Monitor"/>			
+			<meta property="og:description" content="Calcule problemas matemáticos em diversas áreas: ... - O Monitor"/>
 		<?php endif; ?>
 
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
@@ -84,23 +84,17 @@
         </div>
         <!-- End of Search Wrapper -->
         <div class="container">
-				<div class="row">	
+				<div class="row">
 					<div class="span3">
 						<div id='s-menu'>
 							<br><br><br>
 							<?php
-							$items = ExemplosSearchCategoria::model()->findAll(array('order'=>'ordem'));
-							echo '<ul>';
-							foreach($items as $i){
-								echo CHtml::tag('li',array(),CHtml::ajaxLink($i->nome, $this->createUrl('search/exemplos', array('c'=>$i->id)),array(
-									'beforeSenf' => 'js: function() { $("#s-ex").html("..."); }',
-									'success' => 'js: function(html) {
-										$("#s-ex").hide(0).html(html).fadeIn(300);			
-										MathJax.Hub.Queue(["Typeset",MathJax.Hub,document.getElementById("s-ex")]);			
-									}',
-								),array('id' => 'load-s-ex'.$i->id,)));
-							}
-							echo '</ul>';						
+				      $this->renderPartial('//search/menuExemplos', array(
+                'items' => ExemplosSearchCategoria::model()->findAll(array(
+                  'order'=>'ordem',
+                  'condition'=>'pai_id IS NULL',
+                )),
+              ));
 							?>
 						<br><br>
 						<button type="button" class="btn btn-mini btn-card" onclick="$('#main-hist').slideToggle();" >Histórico</button>
@@ -112,7 +106,7 @@
 
 					</div>
 
-				
+
 					<div class="span9">
 						<br>
 						<div id="s-mini-hist">
@@ -126,11 +120,11 @@
 						<div id="s-save" class="textCenter">
 							<?php $this->renderPartial('/search/_saveSearch', array('q' => isset($_GET['q']) ? $_GET['q'] : null)); ?>
 						</div><br><br>
-					</div>        
+					</div>
 				</div>
 		</div>
         <!-- Start of Page Container -->
-        
+
         <!-- End of Page Container -->
 
 
@@ -218,7 +212,7 @@
             </div>
             <!-- End of Footer Bottom -->
 
-        </footer>     
+        </footer>
 
         <!-- End of Footer -->
         <script src="<?php echo Yii::app()->baseUrl; ?>/webroot/monitor/js/main.js" type="text/javascript"></script>
---
+++ protected/views/search/menuExemplos.php
@@ -0,0 +1,17 @@
+<?php
+echo '<ul>';
+foreach($items as $i){
+  echo CHtml::tag('li',array(),CHtml::ajaxLink($i->nome, $this->createUrl('search/exemplos', array('c'=>$i->id)),array(
+    'beforeSenf' => 'js: function() { $("#s-ex").html("..."); }',
+    'success' => 'js: function(html) {
+      $("#s-ex").hide(0).html(html).fadeIn(300);
+      MathJax.Hub.Queue(["Typeset",MathJax.Hub,document.getElementById("s-ex")]);
+    }',
+  ),array('id' => 'load-s-ex'.$i->id,)));
+}
+if(count($i->filhas)>0){
+  echo '<li>';
+  $this->renderPartial('menuExemplos',array('items'=>$i->filhas));
+  echo '</li>';
+}
+echo '</ul>';
