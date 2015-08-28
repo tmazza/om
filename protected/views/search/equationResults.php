01d11cbefc589b8f1862df9316f16e61becdbd6d
davi <davi646@gmail.com>
2015-08-28 09:46:09 -0300

    feedback parcial

--- protected/views/search/equationResults.php
+++ protected/views/search/equationResults.php
@@ -11,6 +11,16 @@ if (!isset($_GET['q']) || (isset($_GET['q']) && strlen($_GET['q']) == 0)) {
     $this->renderPartial('emptySearch');
 }
 ?>
+
+<?php
+
+$instrucao = isset($_GET['q'])?$_GET['q']:'';
+$this->widget('shared.widgets.Feedback.Feedback', array(
+    'topico' => "Instrução<br> ".$instrucao,
+    'autor_nome' => "Luis Gustavo Mendes", 'autor_id' => "19"))
+?>
+
+
 <style>
     <!--
     *, *::before, *::after{
