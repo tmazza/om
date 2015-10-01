7ba4788d85d5e2f5f9c30eda3ce5357e21a2b4f1
tmazza <tiagomdepaula@gmail.com>
2015-10-01 19:57:48 -0300

    pagina da instrucao atualziada

--- themes/material/views/instrucoes/ver.php
+++ themes/material/views/instrucoes/ver.php
@@ -3,27 +3,44 @@
     <h5 class=''>
       <?=$instrucao->descricao;?>
       <?php if(count($instrucao->nomes) > 0): ?>
-        <a href='<?=$this->createUrl('site/index',array('q'=>$instrucao->nomes[0]->id));?>'>
-          <span class="grey-text right tooltipped" data-tooltip='Executar interação.' title='Executar interação.'>
-            <i class='material-icons'>send</i>
-          </span>
-        </a>
       <?php endif; ?>
     </h5>
   </div>
 
   <div class="row">
+    <div class="col s12 m6 l3">
+      <div class="card-panel center-align">
+        <div class="flow-text">
+          Utilizada<br>
+        </div>
+        <b><?=(SearchTerms::model()->count('instrucao = ' . $instrucao->id) + ceil($instrucao->id))?> vez(es)</b>
+      </div>
+    </div>
+    <div class="col s12 m6 l4 right">
+      <div class="right-align flow-text">
+        <a href='<?=$this->createUrl('site/index',array('q'=>$instrucao->nomes[0]->id));?>'>
+          <div class="card-panel center-align">
+            <span class=" tooltipped" data-tooltip='Executar interação.' title='Executar interação.'>
+              Interagir <i class='material-icons right' style="padding-top:8px;">send</i>
+            </span>
+          </div>
+        </a>
+      </div>
+    </div>
+
+  </div>
+
+  <div class="row">
     <div class="col s12">
       <div class="card-panel">
-      <?php foreach ($instrucao->templates as $t): ?>
-          <?=$t->descricao;?>
+        <?php if(count($instrucao->templates) > 0): ?>
+          <?php $t=$instrucao->templates[0];?>
           <div class='sage-auto'>
             <script type="text/sagecell">
-              <?=ShView::mergeDataToTemplate(stripslashes($t->template),array());;?>
+              <?=ShView::mergeDataToTemplate(stripslashes($t->template),array());?>
             </script>
           </div>
-
-      <?php endforeach; ?>
+        <?php endif; ?>
       </div>
     </div>
     <div class="col s12">
