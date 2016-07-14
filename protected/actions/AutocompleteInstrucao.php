82f45e35403b5555b1dde68a992843684c46931e
tmazza <tiagomdepaula@gmail.com>
2016-07-14 20:00:00 -0300

    Ordem na busca por lcs e levenshtein

--- protected/actions/AutocompleteInstrucao.php
+++ protected/actions/AutocompleteInstrucao.php
@@ -8,22 +8,63 @@
 class AutocompleteInstrucao extends CAction {
 
     public function run($term) {
-        $data = [];
+        $term = ShView::normalizarSting($term);
+        $instrucoes = $this->getInstrucoes();
 
-        $exemplos = ExemplosSearch::model()->findAll([
-            'condition' => "valor like '%{$term}%' OR latex like '%{$term}%'",
-            'limit' => 5,
-        ]);
+        foreach ($instrucoes as $k => $i){
+            $instrucoes[$k]['lcs'] = $this->lcs($i['query'], $term);
+            $instrucoes[$k]['levenshtein'] = levenshtein($i['query'], $term);
+        }        
+        uasort($instrucoes,function($a,$b){
+            if($a['lcs'] == $b['lcs'])
+                return $a['levenshtein'] > $b['levenshtein'];
+            else
+                return $a['lcs'] < $b['lcs'];
+        });
         $data = array_map(function($i){
             return [
-                'label'=>'$'.$i->latex.'$',
-                'value'=>$i->valor,
+                'label'=>$i['label'],
+                'value'=>$i['value']
             ];
-        },$exemplos);
+        },array_slice($instrucoes,0,10));
+
         echo json_encode($data);
     }
 
+    private function getInstrucoes(){
+        $instrucoes=Yii::app()->cache->get('instrucoes');
+        if($instrucoes===false) {
+            $instrucoes = array_map(function($i){
+                $words = explode(',',$i->keywords);
+                return [
+                    'label'=>$i->descricao,
+                    'value'=>$words[0], // TODO: join com instrucao nome
+                    'query'=>ShView::normalizarSting($i->descricao),
+                ];
+            },Instrucao::model()->findAll("publicado=1"));
+            Yii::app()->cache->set('instrucoes',$instrucoes,60*2);
+        }
+        return $instrucoes;
+    }
 
+
+    private function lcs($a,$b){
+        $m = strlen($a);
+        $n = strlen($b);
+        $C = [];
+        for($i=0;$i<=$m;$i++) { $C[$i][0] = 0; }
+        for($i=0;$i<=$n;$i++) { $C[0][$i] = 0; }     
+        for($i=1;$i<=$m;$i++){
+          for($j=1;$j<=$n;$j++){
+            if($a[$i-1] == $b[$j-1]){
+              $C[$i][$j] = $C[$i-1][$j-1] + 1;
+            } else {
+              $C[$i][$j] = $C[$i-1][$j] > $C[$i][$j-1] ? $C[$i-1][$j] : $C[$i][$j-1];          
+            }
+          }
+        }
+        return $C[$m][$n];
+    }
 
 
 }
--- protected/helpers/ShView.php
+++ protected/helpers/ShView.php
@@ -138,4 +138,15 @@ class ShView {
         return $data;
     }
 
+
+    public static function normalizarSting($str) {
+        $map = array(
+          'á'=>'a','à'=>'a','ã'=>'a','â'=>'a','é'=>'e','ê'=>'e','í'=>'i','ó'=>'o',
+          'ô'=>'o','õ'=>'o','ú'=>'u','ü'=>'u','ç'=>'c','Á'=>'A','À'=>'A','Ã'=>'A',
+          'Â'=>'A','É'=>'E','Ê'=>'E','Í'=>'I','Ó'=>'O','Ô'=>'O','Õ'=>'O','Ú'=>'U',
+          'Ü'=>'U','Ç'=>'C'
+        );
+        return strtolower(strtr($str, $map));
+    }
+
 }
