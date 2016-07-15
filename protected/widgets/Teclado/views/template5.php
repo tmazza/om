979a8bc265bc3e3b95fe47012509ae8eb895ec38
tmazza <tiagomdepaula@gmail.com>
2016-07-15 18:43:42 -0300

    Fix abre/fecha categorias do teclado

--- protected/widgets/Teclado/views/template5.php
+++ protected/widgets/Teclado/views/template5.php
@@ -1,7 +1,10 @@
-<ul id='tec-btn-box-<?= $this->inputID ?>' class="collapsible" data-collapsible="accordion" style='display:none;'>
+<ul id='tec-btn-box-<?= $this->inputID ?>' class="collapsible" style='display:none;'>
   <?php foreach ($categorias as $c): ?>
       <li>
-        <div class="collapsible-header truncate"><i class="material-icons">add</i><?= $c->nome ?></div>
+        <div class="collapsible-header truncate" onclick="$('.collapsible-body').slideUp();$(this).next().slideDown();">
+          <i class="material-icons">add</i>
+          <?= $c->nome ?>
+        </div>
         <div class="collapsible-body">
           <?php foreach ($c->teclas as $t): ?>
               <a href="#!" class="btn btn-tec blue-grey" style='margin:2px 4px;' data-code="<?= $t->code; ?>">
